f3c6986039d883e96ee930a8dc8597fd5a2d1127
Marcos Antonio de Gois Silva <marcossilvagois@ibm.com>
2022-11-15 17:21:52 -0300

    Criando bucket terraform

---
+++ .github/providers.tf
@@ -0,0 +1,16 @@
+provider "google" {
+    project = var.project_id
+    region  = var.region  
+}
+terraform {
+  backend "gcs"{
+    bucket = "stack-state-data-pipeline-gcp" # bucket criado para armazenar o estado do terraform
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "3.67.0"
+    }
+  }
+}
---
+++ .github/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "stack-data-pipeline-gcp"
+region = "us-centra1"
