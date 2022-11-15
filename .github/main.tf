9b5f7dbcf611b8597d5a4eaa7fb0c756188ccabb
Marcos Antonio de Gois Silva <marcossilvagois@ibm.com>
2022-11-15 18:58:38 -0300

    create bucket terraform

--- .github/main.tf
+++ .github/main.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "gcs_bucket" {
-    name = "teste-bucket-random-987987978"  
+    name = "teste-bucket-random-987987978" 
+    location = var.region 
 }
-# teste1
+# teste2
