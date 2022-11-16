b9ff591c9bb3bef822c579a5621b432c5b3923ee
Marcos Antonio de Gois Silva <marcossilvagois@ibm.com>
2022-11-16 15:57:20 -0300

    Update Creating Bucket Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "gcs_bucket" {
     name = "teste-bucket-random-987987970"
-    location = var.region 
+    location = var.region
+    storage_class = "REGIONAL"
 }
 # teste9
