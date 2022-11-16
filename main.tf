faf2d94c910adfbfe98055edd410f90c994ba38d
Marcos Antonio de Gois Silva <marcossilvagois@ibm.com>
2022-11-16 16:34:25 -0300

    Update Storage Class

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "gcs_bucket" {
-    name = "teste-bucket-random-987987971"
+    name = "teste-bucket-random-987987972"
     location = var.region
     storage_class = var.storage_class
 }
--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "google" {
     project = var.project_id
     region  = var.region
-    
+
 }
 terraform {
   backend "gcs"{
